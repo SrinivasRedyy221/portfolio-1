5063b375fc51d0af1a2114561a58858deecf766f
SrinivasRedyy221 <76741223+SrinivasRedyy221@users.noreply.github.com>
2022-12-02 19:06:59 +0530

    updated

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -35,7 +35,7 @@ const Projects = (props: Props) => {
                 Lorem ipsum, or lipsum as it is sometimes known, is dummy text
                 used in laying out print, graphic or web designs. The passage is
                 attributed to an unknown typesetter in the 15th century who is
-                thought to have scrambled parts of Cicero's De Finibus Bonorum
+                thought to have scrambled parts of Cicero De Finibus Bonorum
                 et Malorum for use in a type specimen book.
               </div>
             </div>
