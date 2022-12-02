b1c940fe410f7aeac3ddc5f487d3648f39efef3f
SrinivasRedyy221 <76741223+SrinivasRedyy221@users.noreply.github.com>
2022-12-02 19:03:20 +0530

    title

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,7 +34,7 @@ const Home:NextPage = () => {
   return (
     <div className="bg-[rgb(36,36,36)] h-screen text-white snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
       <Head>
-        <title>Srinivas's Portfolio</title>
+        <title>Srinivas Portfolio</title>
       </Head>
       <Header />
       <div id="hero" className="snap-start">
