728a20dae553230c5aadc3519bd4cd7576d0e4b4
SrinivasRedyy221 <76741223+SrinivasRedyy221@users.noreply.github.com>
2023-04-19 23:57:58 +0530

    updated

--- components/Skill8.tsx
+++ components/Skill8.tsx
@@ -29,7 +29,7 @@ const Skill8 = ({ directionLeft }: Props) => {
 
       <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0">
         <div className="flex items-center justify-center h-full">
-          <div className="text-3xl font-bold text-black opacity-100">7%</div>
+          <div className="text-3xl font-bold text-black opacity-100">70%</div>
         </div>
       </div>
     </div>
